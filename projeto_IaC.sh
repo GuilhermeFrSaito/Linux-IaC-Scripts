e1cedeeaff79e7aaf6da6c4a1edee5674227428a
GuilhermeFrSaito <guilhermefsaito@gmail.com>
2024-06-14 19:24:08 +0000

    added echo to script

--- projeto_IaC.sh
+++ projeto_IaC.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Criando os diretórios
+echo "Criando os diretórios..."
 
 mkdir /publico
 mkdir /adm
@@ -8,12 +9,14 @@ mkdir /ven
 mkdir /sec
 
 # Criando os grupos
+echo "Criando os grupos..."
 
 groupadd GRP_ADM
 groupadd GRP_VEN
 groupadd GRP_SEC
 
 # Criando os usuarios
+echo "Criando os usuários..."
 
 useradd carlos -m -s /bin/bash -p $(openssl passwd Senha123) -e -G GRP_ADM
 useradd maria -m -s /bin/bash -p $(openssl passwd Senha123) -e -G GRP_ADM
@@ -28,6 +31,7 @@ useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -e -G GRP_SEC
 useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123) -e -G GRP_SEC
 
 # Criando as associações de pastas e grupos
+echo "Criando as associações de pastas e grupos..."
 
 chown root:root /publico
 chown root:GRP_ADM /adm
@@ -35,9 +39,11 @@ chown root:GRP_VEN /ven
 chown root:GRP_SEC /sec
 
 # Modificando as permissões de cada pasta
+echo "Modificando as permissões de pastas..."
 
 chmod 777 /publico
 chmod 770 /adm
 chmod 770 /ven
 chmod 770 /sec
 
+echo "Finalizado!"
